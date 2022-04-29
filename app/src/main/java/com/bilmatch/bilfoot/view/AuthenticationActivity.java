ecfe8f15b62a93eb3f72f560437f8eb60c7aed74
Serhat Merak <merakserhat@gmail.com>
2022-04-29 15:51:52 +0300

    Authentication Screen Design

---
+++ app/src/main/java/com/bilmatch/bilfoot/view/AuthenticationActivity.java
@@ -0,0 +1,16 @@
+package com.bilmatch.bilfoot.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bilmatch.bilfoot.R;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
