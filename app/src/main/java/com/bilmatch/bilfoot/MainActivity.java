71a05f86868138cbde4bc8467c5a6466b22b89b2
Serhat Merak <merakserhat@gmail.com>
2022-05-01 15:33:13 +0300

    Solve conflict

--- app/src/main/java/com/bilmatch/bilfoot/MainActivity.java
+++ app/src/main/java/com/bilmatch/bilfoot/MainActivity.java
@@ -1,22 +1,13 @@
 package com.bilmatch.bilfoot;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.bilmatch.bilfoot.controllers.AuthenticationController;
-import com.bilmatch.bilfoot.view.AuthenticationActivity;
-import com.bilmatch.bilfoot.view.ProfileScreenActivity;
-import com.bilmatch.bilfoot.view.registration.RegistrationUserDefiningsActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.bilmatch.bilfoot.view.registration.PositionSelectionActivity;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,17 +20,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        //Burası daha önceden oturum açılmış mı kontrol eder
-        //FirebaseUser currentUser = mAuth.getCurrentUser();
-        //if(currentUser != null){
-          //  reload();
-        //}
+
 
         authenticationController = new AuthenticationController(mAuth);
-        startActivity(new Intent(this, ProfileScreenActivity.class));
+        //startActivity(new Intent(this, ProfileScreenActivity.class));
+        //startActivity(new Intent(this, AuthenticationActivity.class));
+        startActivity(new Intent(this, PositionSelectionActivity.class));
 
-        //authenticationController.signIn(this,"merakserhat@gmail.com","123456");
-        //authenticationController.register(this,"merakserhat@gmail.com","123456");
 
     }
 }
---
+++ app/src/main/java/com/bilmatch/bilfoot/view/registration/PositionSelectionActivity.java
@@ -0,0 +1,16 @@
+package com.bilmatch.bilfoot.view.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bilmatch.bilfoot.R;
+
+public class PositionSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_position_selection);
+    }
+}
